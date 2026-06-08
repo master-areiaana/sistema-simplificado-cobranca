55f0a7190c8264dd69843838d71f8b9ea89d5880
Areia Ana <portalcore.consult@gmail.com>
2026-06-08 16:41:38 -0300

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Sistema-Simplificado-Cobranca/',
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
